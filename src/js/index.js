6c77fcbc507f795cea464b84fbee88e57484adc8
Victor <justicejesus1237@gmail.com>
2021-09-20 10:46:25 +0300

    fixes done

--- src/js/index.js
+++ src/js/index.js
@@ -2,24 +2,27 @@ import module from "./module";
 import jquery from "jquery";
 window.$ = window.jQuery = jquery;
 import Swal from "sweetalert2";
-
+import mask from "jquery-mask-plugin";
 
 $(() => {
+  $("#phone").mask("+7-000-000-00-00");
+  $("#phone2").mask("+7-000-000-00-00");
+
   //форма пробного начало
-  $('.header_btn').on('click', () => {
-    $('.freeprice').addClass('freeprice_active');
+  $(".header_btn").on("click", () => {
+    $(".freeprice").addClass("freeprice_active");
   });
 
-  $('.close-price_form ').on('click', () => {
-    $('.freeprice').removeClass('freeprice_active');
-  })
+  $(".close-price_form ").on("click", () => {
+    $(".freeprice").removeClass("freeprice_active");
+  });
   //форма пробного конец
 
   //отправка беплатного начало
-  $('#form').on('submit', function (event) {
-    event.preventDefault()
-    let name = $('#name').val().trim();
-    let phone = $('#phone').val().trim();
+  $("#form").on("submit", function (event) {
+    event.preventDefault();
+    let name = $("#name").val().trim();
+    let phone = $("#phone").val().trim();
     const createHtmlForEmail2 = () => {
       return `<div>
       <div>
@@ -28,53 +31,60 @@ $(() => {
       <div>
         phone: <b>${phone}</b>
       </div>
-      </div>`
-    }
+      </div>`;
+    };
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: "Форма отправлена, мы свяжемся с Вами",
+      showConfirmButton: false,
+      timer: 1500,
+    });
 
     const letterData = {
-      to: 'goncharovfigma@gmail.com',
+      to: "justicejesus1237@gmail.com",
       subject: 'Форма "Попробовать бесплатно"',
-      text: 'yo',
-      html: createHtmlForEmail2()
-    }
-
+      text: "yo",
+      html: createHtmlForEmail2(),
+    };
 
     $.ajax({
-      url: 'https://api.42.works/mailer',
-      type: 'POST',
+      url: "https://api.42.works/mailer",
+      type: "POST",
       cache: false,
       data: JSON.stringify(letterData),
       beforeSend: function () {
-        $('#sendForm').prop("disabled", true)
+        $("#sendForm").prop("disabled", true);
       },
       success: function (data) {
         if (!data) {
-          alert('Произошла ошибка')
+          alert("Произошла ошибка");
         } else {
-          $('#form').trigger("reset")
+          $("#form").trigger("reset");
         }
-        $('#sendForm').prop("disabled", false)
+        $("#sendForm").prop("disabled", false);
       },
       contentType: "application/json; charset=utf-8",
     });
   });
-  //отправка бесплатного конец 
+  //отправка бесплатного конец
   //форма подписки начало
-  $('.price_btn').on('click', () => {
-    $('.subscribe').addClass('subscribe_active');
+  $(".price_btn").on("click", () => {
+    $(".subscribe").addClass("subscribe_active");
   });
 
-  $('.close-price_form ').on('click', () => {
-    $('.subscribe').removeClass('subscribe_active');
-  })
+  $(".close-price_form ").on("click", () => {
+    $(".subscribe").removeClass("subscribe_active");
+  });
+
   //форма подписки конец
 
   //отправка подписки начало
-  $('#form2').on('submit', function (event) {
-    console.log('gg');
-    event.preventDefault()
-    let name2 = $('#name2').val().trim();
-    let phone2 = $('#phone2').val().trim();
+  $("#form2").on("submit", function (event) {
+    console.log("gg");
+    event.preventDefault();
+    let name2 = $("#name2").val().trim();
+    let phone2 = $("#phone2").val().trim();
     const createHtmlForEmail2 = () => {
       return `<div>
       <div>
@@ -83,8 +93,8 @@ $(() => {
       <div>
         phone: <b>${phone2}</b>
       </div>
-      </div>`
-    }
+      </div>`;
+    };
 
     // if (name2 == '') {
     //   $('#errorMassage').text("Введите имя")
@@ -95,52 +105,57 @@ $(() => {
     // }
     // $('#errorMassage').text('')
 
-    const letterData = {
-      to: 'goncharovfigma@gmail.com',
-      subject: 'Форма "Подписаться"',
-      text: 'yo',
-      html: createHtmlForEmail2()
-    }
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: "Форма отправлена, мы свяжемся с Вами",
+      showConfirmButton: false,
+      timer: 2500,
+    });
 
+    const letterData = {
+      to: "justicejesus1237@gmail.com",
+      subject: 'Форма "Подписаться"',
+      text: "yo",
+      html: createHtmlForEmail2(),
+    };
 
     $.ajax({
-      url: 'https://api.42.works/mailer',
-      type: 'POST',
+      url: "https://api.42.works/mailer",
+      type: "POST",
       cache: false,
       data: JSON.stringify(letterData),
       beforeSend: function () {
-        $('#sendForm').prop("disabled", true)
+        $("#sendForm").prop("disabled", true);
       },
       success: function (data) {
         if (!data) {
-          alert('Произошла ошибка')
+          alert("Произошла ошибка");
         } else {
-          $('#form2').trigger("reset")
+          $("#form2").trigger("reset");
         }
-        $('#sendForm').prop("disabled", false)
+        $("#sendForm").prop("disabled", false);
       },
       contentType: "application/json; charset=utf-8",
     });
   });
-  //отправка подписки конец 
+  //отправка подписки конец
 
   //подключения кнопок "попробовать бесплатно" начало
-  $('.header_section-btn').on('click', () => {
-    $('.freeprice').addClass('freeprice_active');
+  $(".header_section-btn").on("click", () => {
+    $(".freeprice").addClass("freeprice_active");
   });
 
-  $('.close-price_form ').on('click', () => {
-    $('.freeprice').removeClass('freeprice_active');
-  })
-
-  $('.price-btn').on('click', () => {
-    $('.freeprice').addClass('freeprice_active');
+  $(".close-price_form ").on("click", () => {
+    $(".freeprice").removeClass("freeprice_active");
   });
 
-  $('.close-price_form ').on('click', () => {
-    $('.freeprice').removeClass('freeprice_active');
-    //подключения кнопок конец 
-  })
+  $(".price-btn").on("click", () => {
+    $(".freeprice").addClass("freeprice_active");
+  });
 
-
-})
+  $(".close-price_form ").on("click", () => {
+    $(".freeprice").removeClass("freeprice_active");
+    //подключения кнопок конец
+  });
+});
